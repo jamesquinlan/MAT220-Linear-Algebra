c3f00d4a4398034e90c8b5cc54e9c50de1d16e2a
jamesquinlan <jquinlan@une.edu>
2022-12-29 19:37:40 -0500

    fixed Modified Gram-Schmidt

--- matlab/gram-schmidt.m
+++ matlab/gram-schmidt.m
@@ -1,3 +1,19 @@
+% Test Matrices 
+% -------------------------------------------------------------------------
+% A = [1 -1 4;1 4 -2;1 4 2;1 -1 0];
+% A = [1 -2 -1;2 0 1;2 -4 2;4 0 0];
+% A = [2    -3     1;  1    -2     1; 1    -3     2]
+% A = [12 -51 4;6 167 -68;-4 24 -41];
+A = [
+  0.75126707 0.75126707 0.75126706 0.75126706;
+  0.25509512 0.25509512 0.25509512 0.25509512;
+  0.50595705 0.50595706 0.50595705 0.50595706;
+  0.69907672 0.69907673 0.69907673 0.69907673;
+  0.89090326 0.89090326 0.89090326 0.89090326
+];
+
+
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %% Gram-Schmitt Orthogonalization Process
 %
 % Given matrix A with n independent columns (e.g. rank(A) = n), return
@@ -5,40 +21,47 @@
 % inner products, i.e., R(i,j) = Q(:,i)'*A(:,j).
 %
 % Quinlan, J. 04/23/2022
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 clc
-
-%A = [1 -1 4;1 4 -2;1 4 2;1 -1 0];
-A = [1 -2 -1;2 0 1;2 -4 2;4 0 0];
-disp(A);
 
 [m,n] = size(A);
 Q = zeros(m,n);
 R = zeros(n);
-P = zeros(m,n-1);
-
-% 
-R(1,1) = norm(A(:,1));
-Q(:,1) = A(:,1) ./ R(1,1);
+% P = zeros(m,n-1);
+a = A(:,1);
+ 
+R(1,1) = norm(a);
+Q(:,1) = a ./ R(1,1);
 
 for j = 2:n
-       
+    a = A(:,j);   
     % Compute projection
     for i = 1:j-1
         R(i,j) = Q(:,i)'*A(:,j);
-        P(:,j-1) = P(:,j-1) + R(i,j)*Q(:,i);
+        a = a - R(i,j)*Q(:,i);
     end
-    R(j,j) = norm(A(:,j) - P(:,j-1));
-    Q(:,j) = (A(:,j) - P(:,j-1))./R(j,j);
+    R(j,j) = norm(a);
+    Q(:,j) = a ./R(j,j);
 end
 
-disp(Q);
-isortho(Q)
-disp(R);
+% Print results
+disp('A = ');disp(A);
+disp('Q = ');disp(Q);
+isothogonal = isortho(Q);
+fprintf('Orthogonal = %d\n\n',isothogonal);
+disp('R = ');disp(R);
+
+QR = Q*R;
+disp('QR = ');disp(QR);
+disp('A = QR?');disp(abs(A - QR) < 2*eps);
+% -------------------------------------------------------------------------
 
 
 
 
+
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %% Modified Gram-Schmitt Orthogonalization Algorithm
 %
 % The above QR method does not generally produce accurate results in finite
@@ -50,22 +73,28 @@ disp(R);
 % vectors {a1^(1), a2^(1), ..., an^(1)}.
 % 
 % Quinlan, J. 04/23/2022
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 clc
-A = [1 -1 4;1 4 -2;1 4 2;1 -1 0];
-% A = [1 -2 -1;2 0 1;2 -4 2;4 0 0];
+
 [m,n] = size(A);
 Q = zeros(m,n);
 R = zeros(n);
-AA = A;  % NOTE: A changes within loop so AA is a copy
+a = A(:,1);
 
-for i = 1:n
-    R(i,i) = norm(A(:,i));
-    Q(:,i) = A(:,i) / R(i,i);
-    for j = i+1:n
-       R(i,j) = Q(:,i)'*A(:,j);
-       A(:,j) = A(:,j) - R(i,j)*Q(:,i);
+R(1,1) = norm(a);
+Q(:,1) = a ./ R(1,1);
+
+for j = 2:n
+    a = A(:,j);   
+    % Compute projection
+    for i = 1:j-1
+        R(i,j) = Q(:,i)'*a;
+        a = a - R(i,j)*Q(:,i);
     end
+    R(j,j) = norm(a);
+    Q(:,j) = a ./R(j,j);
 end
 
 % Print results
@@ -76,12 +105,14 @@ fprintf('Orthogonal = %d\n\n',isothogonal);
 disp('R = ');disp(R);
 
 QR = Q*R;
-disp('A = QR?');disp(abs(AA - QR) < eps);
+disp('QR = ');disp(QR);
+
+disp('A = QR?');disp(abs(A - QR) < eps);
 
 
 %{
 NOTES:
--------------------------------
+% -------------------------------------------------------------------------
 Defintion: ||v|| = sqrt(<v,v>)  
      
 Defintion: u and v are orthogonal is <u,v> = 0.  
@@ -94,8 +125,8 @@ Definition: Q is orthogonal if the columns form an orthonormal set
 Theorem: An n x n matrix Q is orthongoal if and only if Q'Q=I
 
 
-REFERENCES:
--------------------------------
+REFERENCES
+---------------------------------------------------------------------------
   Golub, G. H., & Van Loan, C. F. (2012). Matrix computations (Vol. 3). JHU Press.
   Leon, S. (2011). Linear Algebra with Applications (8th).  Pearson.
 %}
